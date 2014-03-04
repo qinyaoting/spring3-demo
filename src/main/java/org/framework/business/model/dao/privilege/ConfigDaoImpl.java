eedf124ae164f30f4fed733a186a908d78842c5d
QinYaoting <qinyaoting@gmail.com>
2014-03-04 16:10:41 +0800

    test

--- src/main/java/org/framework/business/model/dao/privilege/ConfigDaoImpl.java
+++ src/main/java/org/framework/business/model/dao/privilege/ConfigDaoImpl.java
@@ -1,7 +1,18 @@
 package org.framework.business.model.dao.privilege;
 
 import org.framework.business.model.dao.base.BaseDaoImpl;
+import org.framework.xcode.nosql.redis.RedisSpringProxy;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.data.redis.connection.RedisConnection;
+import org.springframework.data.redis.core.RedisCallback;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.stereotype.Repository;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -10,5 +21,6 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public class ConfigDaoImpl extends BaseDaoImpl implements ConfigDao {
+
 
 }
