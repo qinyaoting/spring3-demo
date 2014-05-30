eb5d8257609132a6618133774de8d879086de81d
Qin <qinyaoting@gmail.com>
2014-05-30 22:14:42 +0800

    ...

---
+++ src/main/java/com/xyz/design_pattern/chapter5/Test.java
@@ -0,0 +1,15 @@
+package com.xyz.design_pattern.chapter5;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: qinyaoting
+ * Date: 5/30/14
+ * Time: 10:09 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Test {
+
+    //里氏代换原则：子类型必须能够替换掉他们的父类
+
+    // 只有当子类可以替换掉父类，软件单位的功能不受到影响时，父类才能正在被复用，二子类也能够在父类的基础上增加新的行为
+}
