5060489f9fa8f40313612a161b4e3c130c8ee883
QinYaoting <qinyaoting@gmail.com>
2014-05-30 12:01:17 +0800

    strategy

---
+++ src/main/java/com/xyz/design_pattern/chapter2/CashNormal.java
@@ -0,0 +1,15 @@
+package com.xyz.design_pattern.chapter2;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: vuclip123
+ * Date: 5/30/14
+ * Time: 11:24 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class CashNormal extends CashSuper {
+    @Override
+    public double acceptCash(double money) {
+        return money;
+    }
+}
---
+++ src/main/java/com/xyz/design_pattern/chapter2/optimize/Strategy.java
@@ -0,0 +1,13 @@
+package com.xyz.design_pattern.chapter2.optimize;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: vuclip123
+ * Date: 5/30/14
+ * Time: 11:33 AM
+ * To change this template use File | Settings | File Templates.
+ */
+abstract class Strategy {
+
+    public abstract void algorithmInterface();
+}
