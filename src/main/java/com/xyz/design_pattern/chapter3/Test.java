903f53817922d8af98d57832d5b9b202e409d28b
Qin <qinyaoting@gmail.com>
2014-05-30 22:03:22 +0800

    The Open-Closed Principle

---
+++ src/main/java/com/xyz/design_pattern/chapter3/Test.java
@@ -0,0 +1,19 @@
+package com.xyz.design_pattern.chapter3;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: qinyaoting
+ * Date: 5/30/14
+ * Time: 9:44 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Test {
+
+    //单一职责原则：就一个类而言，应该仅有一个引起他变化的原因
+
+    //如果一个类承担的职责过多，就等于把这些职责耦合在一起，
+    // 一个职责的变化可能会削弱或抑制这个类完成其他职责的能力。
+    // 这种耦合设计导致脆弱的设计，当变化发生时，设计会遭到意想不到的破会
+
+    // 软件设计真正要做的许多内容，就是发现职责并把那些职责相互分离
+}
---
+++ src/main/java/com/xyz/design_pattern/chapter4/Test.java
@@ -0,0 +1,20 @@
+package com.xyz.design_pattern.chapter4;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: qinyaoting
+ * Date: 5/30/14
+ * Time: 9:52 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Test {
+
+    // 开放-封闭原则(The Open-Closed Principle)：是说软件实体（类，模块，函数等）应该可以扩展，但是不可修改
+
+    // 面对需求，对程序的改动是通过增加新代码进行的，而不是更改现有的代码，是开放封闭原则的精神所在
+
+    // 面向对象的核心所在，可维护，可扩展，可复用，灵活性好
+    // 开发人员应该近对程序中呈现出频繁变化的那些播放做出抽象，然而，对应应用程序中的每个部分都刻意进行抽象同样不是一个好主意
+    // 拒绝不成熟的抽象和抽象本身一样重要
+
+}
