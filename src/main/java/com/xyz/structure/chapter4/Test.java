83d29f74b10946bff9b638197c88e8e06c0ed3f4
QinYaoting <qinyaoting@gmail.com>
2014-06-12 14:44:33 +0800

    stack

---
+++ src/main/java/com/xyz/structure/chapter4/Test.java
@@ -0,0 +1,15 @@
+package com.xyz.structure.chapter4;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: vuclip123
+ * Date: 6/12/14
+ * Time: 2:37 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Test {
+
+    // Stack 限定在表尾进行插入和删除操作的线性表
+
+    // LIFO
+}
