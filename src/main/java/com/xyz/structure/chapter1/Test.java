0eafde79f13a68fc50ab2ba102c6f50a39e021de
Qin <qinyaoting@gmail.com>
2014-06-14 11:37:23 +0800

    test

---
+++ src/main/java/com/xyz/structure/chapter1/Test.java
@@ -0,0 +1,32 @@
+package com.xyz.structure.chapter1;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: qinyaoting
+ * Date: 6/14/14
+ * Time: 10:04 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Test {
+
+    // 数据 计算机能加工的
+
+    // 数据元素 组成数据的，有一定意义的基本单位，在计算机系统通常作为整体处理人类有人组成r
+
+    // 数据项 一个数据元素可以有若干个数据项组成 是数据不可分割的最小单位 人有耳朵鼻子嘴
+
+    // 数据对象 性质相同的数据元素的集合，是数据的子集
+
+    // 数据结构 相互之间存在一种或多种特定关系的数据元素的集合
+
+
+    // 逻辑结构 ：数据对象中数据元素之间的相互关系
+
+    // 集合结构 线性结构 树形结构 图形结构
+
+    // 物理结构 如何将数据元素存储到计算机的内存中 顺序存储结构 链式存储结构
+
+    // 数据类型 一组性质相同的值的集合及定义在次集合上的一些操作
+
+    // 抽象数据类型 adt abstract data type
+}
