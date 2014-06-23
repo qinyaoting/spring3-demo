a78e532ec4814e9ff84613953820b89573dd642b
QinYaoting <qinyaoting@gmail.com>
2014-06-23 16:15:51 +0800

    test

--- src/main/java/com/xyz/structure/chapter4/Test.java
+++ src/main/java/com/xyz/structure/chapter4/Test.java
@@ -9,7 +9,43 @@ package com.xyz.structure.chapter4;
  */
 public class Test {
 
-    // Stack 限定在表尾进行插入和删除操作的线性表
+    // 4.2.1 栈的定义 Stack 限定在表尾进行插入和删除操作的线性表
 
-    // LIFO
+    // LIFO (Last In First Out)
+
+    //4.2.2 进栈出栈变化形式
+
+    // 4.3 栈的抽象数据类型
+
+    // 4.4.1 栈的顺序存储结构
+
+
+
+/*typedef int SElemType;
+
+typedef struct {
+        SElemType data[MAXSIZE];
+        int top;
+} SqlStack;*/
+
+    // 4.4.2 栈的顺序存储结构 - 进栈
+    // 4.4.2 栈的顺序存储结构 - 出栈
+    // 4.5 两栈共享
+    // 4.6.1 栈的链式存储结构
+    // 4.6.2 栈的链式存储结构 - 进栈
+    // 4.6.3 栈的链式存储结构 - 出栈
+    // 4.7 栈的应用
+    // 4.8 栈的应用 - 递归
+    // 4.8.1  斐波那契数列
+
+    public int fbi(int i) {
+        if(i<2) {
+            return i==0?0:1;
+
+        }
+        return fbi(i-1) + fbi(i-2);
+    }
+
+
+
 }
